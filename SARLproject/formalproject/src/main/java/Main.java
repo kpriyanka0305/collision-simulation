7740013746866013f50b9d52950e12ab161f5893
Markus Klinik <mkl@lambdanaut.net>
2019-09-21 09:37:47 +0200

    code formatting

--- SARLproject/formalproject/src/main/java/Main.java
+++ SARLproject/formalproject/src/main/java/Main.java
@@ -15,51 +15,50 @@ import de.tudresden.ws.container.SumoStringList;
 // Hello
 
 public class Main implements Observer {
-    static String sumo_bin = "sumo-gui";
-    static String config_file = "data/cross.sumocfg";
-    static double step_length = 0.2;
+	static String sumo_bin = "sumo-gui";
+	static String config_file = "data/cross.sumocfg";
+	static double step_length = 0.2;
 
 	SumoTraciConnection conn;
 
-    public static void main(String[] args) throws Exception {
-        SumoTraciConnection connection = SumoConnect();
+	public static void main(String[] args) throws Exception {
+		SumoTraciConnection connection = SumoConnect();
 
-        subscribe(connection);
+		subscribe(connection);
 
-        SREBootstrap bootstrap = SRE.getBootstrap();
-        bootstrap.startAgent(WarningService.class, connection);
+		SREBootstrap bootstrap = SRE.getBootstrap();
+		bootstrap.startAgent(WarningService.class, connection);
 //        bootstrap.startAgent(Chaos.class, connection);
-        //bootstrap.startAgent(OnlyRSUWithCamera.class, connection);
-    }
+		// bootstrap.startAgent(OnlyRSUWithCamera.class, connection);
+	}
 
-    public static SumoTraciConnection SumoConnect() throws Exception{
-        SumoTraciConnection conn = new SumoTraciConnection(sumo_bin, config_file);
-        conn.addOption("step-length", step_length+"");
-        conn.addOption("start", "false"); //start sumo immediately
-        conn.addOption("log", "data/log.txt");
-        conn.runServer();
-        conn.setOrder(1);
-        return conn;
-    }
-    
-    public static void subscribe(SumoTraciConnection conn) throws Exception
-    {
-        conn.addObserver(new Main());
-        VariableSubscription vs = new VariableSubscription(SubscribtionVariable.simulation, 0, 100000 * 60, "");
-        vs.addCommand(Constants.VAR_DEPARTED_VEHICLES_IDS);
-        conn.do_subscription(vs);
-    }
+	public static SumoTraciConnection SumoConnect() throws Exception {
+		SumoTraciConnection conn = new SumoTraciConnection(sumo_bin, config_file);
+		conn.addOption("step-length", step_length + "");
+		conn.addOption("start", "false"); // start sumo immediately
+		conn.addOption("log", "data/log.txt");
+		conn.runServer();
+		conn.setOrder(1);
+		return conn;
+	}
+
+	public static void subscribe(SumoTraciConnection conn) throws Exception {
+		conn.addObserver(new Main());
+		VariableSubscription vs = new VariableSubscription(SubscribtionVariable.simulation, 0, 100000 * 60, "");
+		vs.addCommand(Constants.VAR_DEPARTED_VEHICLES_IDS);
+		conn.do_subscription(vs);
+	}
 
 	@Override
 	public void update(Observable arg0, SubscriptionObject so) {
 		if (so.response == ResponseType.SIM_VARIABLE) {
-            assert(so.variable == Constants.VAR_DEPARTED_VEHICLES_IDS);
-            SumoStringList ssl = (SumoStringList) so.object;
-            if (ssl.size() > 0) {
-                for (String vehID : ssl) {
-                    System.out.println("Subscription Departed vehicles: " + vehID);
-                }
-            }
-        }
+			assert (so.variable == Constants.VAR_DEPARTED_VEHICLES_IDS);
+			SumoStringList ssl = (SumoStringList) so.object;
+			if (ssl.size() > 0) {
+				for (String vehID : ssl) {
+					System.out.println("Subscription Departed vehicles: " + vehID);
+				}
+			}
+		}
 	}
 }
