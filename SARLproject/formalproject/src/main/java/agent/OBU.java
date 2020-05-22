6f114ec4f76544c918ab212889fcf5c4a06b35e0
Markus Klinik <mkl@lambdanaut.net>
2020-05-22 11:55:31 +0200

    add agent OBU

---
+++ SARLproject/formalproject/src/main/java/agent/OBU.java
@@ -0,0 +1,60 @@
+package agent;
+
+import de.tudresden.sumo.cmd.Vehicle;
+import de.tudresden.ws.container.SumoColor;
+import it.polito.appeal.traci.SumoTraciConnection;
+
+public class OBU {
+	// Location of the OBUs
+	private String name;
+	private String type;
+	private SumoTraciConnection conn;
+
+	public OBU(String name, SumoTraciConnection conn, String type) {
+		this.name = name;
+		this.conn = conn;
+		this.type = type;
+//		emit(new OBUConnect(this.name))
+	}
+
+	void OBUDisconnect(String name) {
+		if (name.equals(this.name)) {
+//			killMe
+		}
+	}
+
+	void CleanOBU(String name) throws Exception {
+		if (name.equals(this.name)) {
+			goDefaultColor();
+		}
+	}
+
+	void tempClean() throws Exception {
+		goDefaultColor();
+	}
+
+	void WarnOBU(String name) throws Exception {
+		if (name.equals(this.name)) {
+			goRed();
+		}
+	}
+
+	void goRed() throws Exception {
+		this.conn.do_job_set(Vehicle.setColor(this.name, new SumoColor(255, 0, 0, 255)));
+		this.conn.do_job_set(Vehicle.setSpeed(this.name, 0.0));
+	}
+
+	void goDefaultColor() throws Exception {
+		if (type.contains("reckless")) {
+			this.conn.do_job_set(Vehicle.setColor(this.name, new SumoColor(255, 140, 0, 255)));
+		} else {
+			this.conn.do_job_set(Vehicle.setColor(this.name, new SumoColor(255, 255, 0, 255)));
+		}
+		this.conn.do_job_set(Vehicle.setSpeed(this.name, 8.3));
+	}
+
+//	on Destroy
+//	{
+//		println(this.name + " OBU destroyed")	
+//	}
+}
