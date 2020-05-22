a05a0794b99b06eaa60db4cdb9ab36ed2a221123
Markus Klinik <mkl@lambdanaut.net>
2020-05-22 11:22:15 +0200

    translate WarningService and Camera to java

---
+++ SARLproject/formalproject/src/main/java/simulations/Camera.java
@@ -0,0 +1,162 @@
+package simulations;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import de.tudresden.sumo.cmd.Polygon;
+import de.tudresden.sumo.cmd.Vehicle;
+import de.tudresden.ws.container.SumoColor;
+import de.tudresden.ws.container.SumoGeometry;
+import de.tudresden.ws.container.SumoPosition2D;
+import it.polito.appeal.traci.SumoTraciConnection;
+import math.geom2d.Point2D;
+import math.geom2d.polygon.Polygon2D;
+import math.geom2d.polygon.SimplePolygon2D;
+
+public class Camera {
+
+	private String name;
+	private int angle; // 90 - NORTH, 180 - WEST, 270 - SOUTH, 0 - EAST
+	private double height; // Meters
+	private double x; // LOCATION ON THE MAP X
+	private double y; // LOCATION ON THE MAP Y
+	private double size; // Rectangle size
+	private int r = 14;
+	private int g = 214;
+	private int b = 197;
+
+	private Polygon2D fieldOfView;
+
+	// FOR SUMO DRAWING
+	private SumoGeometry cameraObject = new SumoGeometry();
+	private SumoGeometry fovObject = new SumoGeometry();
+
+	// FOR COLLISION
+	private SumoPosition2D center;
+	private List<String> vehicles;
+
+	// CONNECTION OF COURSE
+	private SumoTraciConnection conn;
+
+	public Camera(String name, SumoTraciConnection conn, double x, double y, double size, double height, int angle) throws Exception {
+		// BASE
+		this.name = name;
+		this.conn = conn;
+
+		// THESE ARE FOR CAMERA SQUARE
+		this.x = x;
+		this.y = y;
+		this.size = size;
+
+		// THESE ARE FOR FIELD OF VIEW
+		this.height = height;
+		this.angle = angle;
+
+		// THESE ARE FOR COLLISION POINT
+		this.center = new SumoPosition2D(2.28, 0.88);
+		drawCamera();
+		drawFOV();
+
+		// CONNECT TO THE CONTROLLER
+//		emit(new CameraConnect(this))
+	}
+
+	public void observeSituation() throws Exception {
+		this.vehicles = (List<String>) (this.conn.do_job_get(Vehicle.getIDList()));
+
+		Map<String, Map<String, Object>> vehicle_data = new HashMap<>();
+		for (String v : this.vehicles) {
+			SumoPosition2D sumoPosition = (SumoPosition2D) (this.conn.do_job_get(Vehicle.getPosition(v)));
+			Point2D position = new Point2D(sumoPosition.x, sumoPosition.y);
+
+			if (fieldOfView.contains(position)) {
+				vehicle_data.put(v, readData(v, position));
+			}
+		}
+//		emit(new SendAllDataCamera(vehicle_data))
+	}
+
+	Map<String, Object> readData(String id, Point2D position) throws Exception {
+		String type = (String) (this.conn.do_job_get(Vehicle.getTypeID(id)));
+		double speed = (Double) (this.conn.do_job_get(Vehicle.getSpeed(id)));
+		double length = (Double) (this.conn.do_job_get(Vehicle.getLength(id)));
+		double accel = (Double) (this.conn.do_job_get(Vehicle.getAccel(id)));
+		Point2D cameraLocation = new Point2D(this.center.x, this.center.y);
+		double distance = cameraLocation.distance(position);
+		double seconds;
+
+		distance = distance - length / 2;
+
+		if (speed > 0) {
+			seconds = distance / speed;
+		} else {
+			seconds = 100.0;
+		}
+
+		Map<String, Object> myMap = new HashMap<>();
+		myMap.put("type", type);
+		myMap.put("speed", speed);
+		myMap.put("accel", accel);
+		myMap.put("length", length);
+		myMap.put("distance", distance);
+		myMap.put("seconds", seconds);
+		return myMap;
+	}
+
+//	on ShutdownSimulation
+//	{
+//		val task = task("camera observing")
+//		task.cancel()
+//		killMe
+//	}
+
+//	on Destroy
+//	{
+//		println("Camera destroyed")
+//	}
+
+	// FOR SIMULATION/COLLISION PURPOSES ONLY
+	void drawCamera() throws Exception {
+		this.cameraObject.add(new SumoPosition2D(x + size / 2, y + size / 2));
+		this.cameraObject.add(new SumoPosition2D(x - size / 2, y + size / 2));
+		this.cameraObject.add(new SumoPosition2D(x - size / 2, y - size / 2));
+		this.cameraObject.add(new SumoPosition2D(x + size / 2, y - size / 2));
+
+		this.conn.do_job_set(
+				Polygon.add(this.name, cameraObject, new SumoColor(this.r, this.g, this.b, 255), true, "Square", -2));
+	}
+
+	void drawFOV() throws Exception {
+		double smallRadius = size / 2 + 15;
+		double bigRadius = size / 2 + 15 + (90 * Math.sqrt(this.height));
+
+		double fovAngle = 25;
+
+		// NECESSARY CALCULATIONS
+		double xSmallOne = x + Math.cos(Math.toRadians(angle + fovAngle)) * smallRadius;
+		double ySmallOne = y + Math.sin(Math.toRadians(angle + fovAngle)) * smallRadius;
+
+		double xSmallTwo = x + Math.cos(Math.toRadians(angle - fovAngle)) * smallRadius;
+		double ySmallTwo = y + Math.sin(Math.toRadians(angle - fovAngle)) * smallRadius;
+
+		double xBigOne = x + Math.cos(Math.toRadians(angle - fovAngle)) * bigRadius;
+		double yBigOne = y + Math.sin(Math.toRadians(angle - fovAngle)) * bigRadius;
+
+		double xBigTwo = x + Math.cos(Math.toRadians(angle + fovAngle)) * bigRadius;
+		double yBigTwo = y + Math.sin(Math.toRadians(angle + fovAngle)) * bigRadius;
+
+		// DRAWING OBJECT
+		this.fovObject.add(new SumoPosition2D(xSmallOne, ySmallOne));
+		this.fovObject.add(new SumoPosition2D(xSmallTwo, ySmallTwo));
+		this.fovObject.add(new SumoPosition2D(xBigOne, yBigOne));
+		this.fovObject.add(new SumoPosition2D(xBigTwo, yBigTwo));
+
+		// FOR COLLISION
+		this.fieldOfView = new SimplePolygon2D(new Point2D(xSmallOne, ySmallOne), new Point2D(xSmallTwo, ySmallTwo),
+				new Point2D(xBigOne, yBigOne), new Point2D(xBigTwo, yBigTwo));
+
+		this.conn.do_job_set(
+				Polygon.add(this.name + "FOV", fovObject, new SumoColor(this.r, this.g, this.b, 64), true, "FOV", -2));
+	}
+}
---
+++ SARLproject/formalproject/src/main/java/simulations/SimWarningService.java
@@ -0,0 +1,129 @@
+package simulations;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
+
+import de.tudresden.sumo.cmd.Junction;
+import de.tudresden.sumo.cmd.Vehicle;
+import de.tudresden.ws.container.SumoPosition2D;
+import it.polito.appeal.traci.SumoTraciConnection;
+import kpi.Kpi;
+
+public class SimWarningService implements Simulation {
+
+	private SumoTraciConnection conn;
+	private List<String> OBusList = new ArrayList<String>();
+	private Map<String, Boolean> RsusStatus = new HashMap<String, Boolean>();
+	private double rsu_distance = 13.0;
+	private double cyclist_range = 5.0;
+
+	private Kpi kpis;
+
+	private Consumer<Void> shutdownCallback;
+
+	public SimWarningService(SumoTraciConnection conn, Kpi kpis, Consumer<Void> shutdownCallback) {
+		this.conn = conn;
+		this.kpis = kpis;
+		this.shutdownCallback = shutdownCallback;
+
+		this.RsusStatus.put("East", false);
+		this.RsusStatus.put("West", false);
+
+//		spawnElements
+
+//		run
+	}
+
+	void RSUStatus(String name, boolean status) {
+		if (name.equals("East")) {
+			this.RsusStatus.put("East", status);
+		} else {
+			this.RsusStatus.put("West", status);
+		}
+	}
+
+	@Override
+	public void step() throws Exception {
+		this.conn.do_timestep();
+		List<String> vehicles = (List<String>)(this.conn.do_job_get(Vehicle.getIDList()));
+		
+		if( vehicles.isEmpty() )
+		{
+//			emit(new ShutdownSimulation)
+//			return
+		}
+
+		kpis.checkKPIs();
+
+		for (String v : vehicles) {
+			Map<String,Object> veh_data = readData(v);
+			String type = (String)(veh_data.get("type"));
+			if (type.contains("bus")) {
+				if (!this.OBusList.contains(v)) {
+//					spawn(OBU, v, this.conn)
+					this.OBusList.add(v);
+					System.out.println(v + " ENTERED");
+				}
+			} else if (type.contains("bicycle-distracted")) {	
+				double distance = (Double)(veh_data.get("distance"));
+				double speed = (Double)(veh_data.get("speed"));
+				boolean east_rsu = (Boolean)(this.RsusStatus.get("East"));
+				String road_id = (String)(veh_data.get("road_id"));
+				if (east_rsu && road_id.contains("i")) {
+					if (distance > rsu_distance && distance < rsu_distance + cyclist_range) {
+						this.conn.do_job_set(Vehicle.setSpeed(v, 0.0));
+						//println("Bicycle distracted entered" + distance + "RSU distance is:-" + rsu_distance + "Cyclists range is :- " + cyclist_range + "cyclists speed :-" + speed);
+					}
+				} else {
+					if (speed == 0) {
+						this.conn.do_job_set(Vehicle.setSpeed(v, 4.2)); // 4.2 - speed of bicycle set here
+						//println(v + "Bicycle distracted entered");
+					}
+				}
+			}
+		}
+
+		for (String k : this.OBusList) {
+			boolean flag = false;
+
+			for (String v : vehicles) {
+				if (k.equals(v)) {
+					flag = true;
+					break;
+				}
+			}
+
+			if (!flag) {
+				System.out.println(k + " REMOVED");
+//				emit(new OBUDisconnect(k))
+				this.OBusList.remove(k);
+				break;
+			}
+		}
+	}
+
+	private Map<String, Object> readData(String id) throws Exception {
+		SumoPosition2D position = (SumoPosition2D) (this.conn.do_job_get(Vehicle.getPosition(id)));
+		SumoPosition2D centre = (SumoPosition2D) (this.conn.do_job_get(Junction.getPosition("0")));
+		String type = (String) (this.conn.do_job_get(Vehicle.getTypeID(id)));
+		double speed = (Double) (this.conn.do_job_get(Vehicle.getSpeed(id)));
+		double length = (Double) (this.conn.do_job_get(Vehicle.getLength(id)));
+		double accel = (Double) (this.conn.do_job_get(Vehicle.getAccel(id)));
+		String road_id = (String) (this.conn.do_job_get(Vehicle.getRoadID(id)));
+		double tempx = Math.abs(centre.x - position.x);
+		double tempy = Math.abs(centre.y - position.y);
+		double distance = Math.sqrt(tempx * tempx + tempy * tempy);
+		distance = distance - length / 2;
+		Map<String, Object> myMap = new HashMap<>();
+		myMap.put("type", type);
+		myMap.put("speed", speed);
+		myMap.put("accel", accel);
+		myMap.put("distance", distance);
+		myMap.put("road_id", road_id);
+		myMap.put("length", length);
+		return myMap;
+	}
+}
